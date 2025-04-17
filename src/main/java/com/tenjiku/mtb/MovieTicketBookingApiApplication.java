687189853985e0bd676771548c142bcdeb3be268
Hemanth0701 <hemanthkumar0701m@gmail.com>
2025-04-17 16:59:42 +0530

    intial commit

---
+++ src/main/java/com/tenjiku/mtb/MovieTicketBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.tenjiku.mtb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketBookingApiApplication.class, args);
+	}
+
+}
