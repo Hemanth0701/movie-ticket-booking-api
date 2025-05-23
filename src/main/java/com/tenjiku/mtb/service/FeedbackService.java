7b6d46fb7dde28f3d8b4fbdc2e1599a0e8eac851
Hemanth0701 <hemanthkumar0701m@gmail.com>
2025-05-23 23:12:42 +0530

    feedback creation for user feedback

---
+++ src/main/java/com/tenjiku/mtb/service/FeedbackService.java
@@ -0,0 +1,4 @@
+package com.tenjiku.mtb.service;
+
+public interface FeedbackService {
+}
---
+++ src/main/java/com/tenjiku/mtb/service/Impl/FeedbackServiceImp.java
@@ -0,0 +1,6 @@
+package com.tenjiku.mtb.service.Impl;
+
+import com.tenjiku.mtb.service.FeedbackService;
+
+public class FeedbackServiceImp implements FeedbackService {
+}
